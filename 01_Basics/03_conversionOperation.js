3850e5ea5c72b983ed184e9b306e88c93ed9880e
Bijendra312 <bijendra.saha@gmail.com>
2025-07-07 18:27:53 +0530

    Conversion of data types

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,12 +1,25 @@
 let score="Bijendra"; // string
 
-console.log(typeof score); // number
-console.log(typeof (score)); // number
+// console.log(typeof score); // number
+// console.log(typeof (score)); // number
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber); // number
-console.log(valueInNumber); // NaN
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // NaN
 
 
 //"33" -> 33
 //"33abc" -> NaN
 //true -> 1; false -> 0;
+
+
+let isLoggedIn = "Bijendra"
+let isLoggedInValue = Boolean(isLoggedIn);
+// console.log(isLoggedInValue); // true
+// 1=> true
+// 0 => false
+//"" => false
+//"Bijendra" => true
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber); // "33"
+console.log(typeof stringNumber); // string
