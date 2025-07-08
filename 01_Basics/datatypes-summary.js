137837c0e5e3322b93763871bbe84a86b44c3904
Bijendra312 <bijendra.saha@gmail.com>
2025-07-08 13:51:01 +0530

    learning about datatypes

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,17 @@
+// Primitive
+// Number, String, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100; // Number
+const scoreValue = 100.5; // Number
+const isLoggedIn = true; // Boolean
+const outsideTemp = null; // Null
+let userName; // Undefined
+
+const uniqueId = Symbol('id'); // Symbol
+const anotherUniqueId = Symbol('id'); // Different Symbol
+console.log(uniqueId === anotherUniqueId); // false
+
+
+
+// Reference(Non-Primitive)
+// Object, Array, Function
