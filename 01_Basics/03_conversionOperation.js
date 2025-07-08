2184704df8bcb5c378f8c3029c4748efac0bf903
Bijendra312 <bijendra.saha@gmail.com>
2025-07-08 13:11:20 +0530

    conversions and comparisons

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -21,5 +21,31 @@ let isLoggedInValue = Boolean(isLoggedIn);
 //"Bijendra" => true
 let someNumber = 33
 let stringNumber = String(someNumber);
-console.log(stringNumber); // "33"
-console.log(typeof stringNumber); // string
+// console.log(stringNumber); // "33"
+// console.log(typeof stringNumber); // string
+
+
+//******************************Operations**********************************
+
+let value=3
+let negValue = -value;
+// console.log(negValue); // -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/3);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = "Bijendra";
+let str3 = str1 + " " + str2;
+// console.log(str3); // "hello Bijendra"
+
+// console.log("1" + 2); // "12"
+// console.log(1 + "2"); // "12"
+// console.log("1" + 2+2); // "122"
+// console.log(1 + 2 + "2"); // "32"
+console.log(+true); // 1
+console.log(+"");
