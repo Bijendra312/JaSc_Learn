20f7fb1f01ca399e0f1aae878f6c946b03cb9cae
Bijendra312 <bijendra.saha@gmail.com>
2025-07-15 01:40:25 +0530

    learning about non-primitive datatypes

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -9,9 +9,23 @@ let userName; // Undefined
 
 const uniqueId = Symbol('id'); // Symbol
 const anotherUniqueId = Symbol('id'); // Different Symbol
-console.log(uniqueId === anotherUniqueId); // false
+// console.log(uniqueId === anotherUniqueId); // false
 
 
 
 // Reference(Non-Primitive)
 // Object, Array, Function
+
+const heroes = ['Batman', 'Superman', 'Wonder', 'Flash']; // Array
+const user = {
+  name: 'John',
+  age: 30,
+  isAdmin: false,
+}; // Object
+const greet = function() { // Function
+  console.log('Hello!');
+};
+
+console.log(typeof heroes); // "object"
+console.log(typeof user); // "object"   
+console.log(typeof greet); // "function"
