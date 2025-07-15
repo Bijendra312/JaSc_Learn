4dece04222827dd3756d12c6716beeb8f8fe01fb
Bijendra312 <bijendra.saha@gmail.com>
2025-07-15 19:26:07 +0530

    learning about strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,14 @@
+const name= "Bijendra"; // String
+const repoCount = 10; // Number
+// console.log(`Hello ${name}, your repo count is ${repoCount}`); // Template Literal
+
+const gameName = new String("Cricket"); // String Object
+// console.log(gameName[0]); // "C"
+
+// console.log(gameName.__proto__); // String.prototype
+
+// console.log(gameName.length); // 7
+// console.log(gameName.toUpperCase()); // "CRICKET"
+
+console.log(gameName.charAt(0)); // "C"
+console.log(gameName.indexOf('i')); // 2
