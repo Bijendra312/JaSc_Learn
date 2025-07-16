96e8e9647756ebe6a804eb11ea9e1e0e51302de5
Bijendra312 <bijendra.saha@gmail.com>
2025-07-16 15:15:04 +0530

    methods in strings-part 1

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -1,4 +1,4 @@
-const name= "Bijendra"; // String
+const name= "Bijendra Saha"; // String
 const repoCount = 10; // Number
 // console.log(`Hello ${name}, your repo count is ${repoCount}`); // Template Literal
 
@@ -10,5 +10,20 @@ const gameName = new String("Cricket"); // String Object
 // console.log(gameName.length); // 7
 // console.log(gameName.toUpperCase()); // "CRICKET"
 
-console.log(gameName.charAt(0)); // "C"
-console.log(gameName.indexOf('i')); // 2
+// console.log(gameName.charAt(0)); // "C"
+// console.log(gameName.indexOf('i')); // 2
+
+// console.log(name.includes("Saha")); 
+// console.log(name.slice(1, 4)); // "ije"
+// console.log(name.substring(1, 4)); // "ije"
+// console.log(name.slice(10, 4));// ""
+// console.log(name.substring(10, 4)); 
+
+// console.log(name.split(" ")); // ["Bijendra", "Saha"]
+// console.log(name.split("")); // ["B", "i", "j", "e", "n", "d", "r", " ", "S", "a", "h", "a"]
+// console.log(name.split("e")); // ["Bij", "ndra Saha"]
+// console.log(name.replace("Saha", "Singh")); // "Bijendra Singh"
+
+let sentence = "Apples are sweet. I love apples!";
+let result = sentence.replace(/apples/gi, "mangoes");
+console.log(result); // "Mangoes are sweet. I love mangoes!"
